3a5d4e174ec900511f43b0eb2e7615c70b991632
Wadaboa <falai.alessio@gmail.com>
2021-01-13 11:46:24 +0000

    :construction: add dev set

--- dataset.py
+++ dataset.py
@@ -1,5 +1,6 @@
 import json
 import os
+import uuid
 from functools import partial
 
 import numpy as np
@@ -15,7 +16,7 @@ class SquadDataset:
 
     NAME = "squad"
     COLUMNS = [
-        "index",
+        "question_id",
         "question",
         "title",
         "context_id",
@@ -26,55 +27,63 @@ class SquadDataset:
     ]
     JSON_RECORD_PATH = ["data", "paragraphs", "qas", "answers"]
 
+    DATA_FOLDER = os.path.join(os.getcwd(), "data")
+    TRAIN_DATA_FOLDER = os.path.join(DATA_FOLDER, "training")
+    TRAIN_SET_PATH = os.path.join(TRAIN_DATA_FOLDER, "training_set.json")
+    TRAIN_DF_PATH = os.path.join(TRAIN_DATA_FOLDER, f"{NAME}_train_df.pkl")
+    TEST_DATA_FOLDER = os.path.join(DATA_FOLDER, "testing")
+    TEST_SET_PATH = os.path.join(TEST_DATA_FOLDER, "test_set.json")
+    TEST_DF_PATH = os.path.join(TEST_DATA_FOLDER, f"{NAME}_test_df.pkl")
+
     def __init__(
-        self,
-        df_percentage=1.0,
-        val_split=0.2,
+        self, df_percentage=1.0, val_split=0.2,
     ):
         # Save instance variables
         self.df_percentage = df_percentage
         self.val_split = val_split
 
-        # Create directories
-        self.dataset_folder = os.path.join(os.getcwd(), "data", "training")
-        self.training_set_path = os.path.join(self.dataset_folder, "training_set.json")
-        self.dataframe_path = os.path.join(self.dataset_folder, f"{self.NAME}_df.pkl")
         assert os.path.exists(
-            self.training_set_path
+            self.TRAIN_SET_PATH
         ), "Missing SQuAD training set .json file"
+        assert os.path.exists(
+            self.TEST_SET_PATH
+        ), "Missing SQuAD testing set .json file"
 
         # Prepare the dataset and load the dataframe
-        if not os.path.exists(self.dataframe_path):
-            df = self._load_dataset()
-        else:
-            df = pd.read_pickle(self.dataframe_path)
-        
+        train_df = self._load_dataset(self.TRAIN_SET_PATH, self.TRAIN_DF_PATH)
+        test_df = self._load_dataset(self.TEST_SET_PATH, self.TEST_DF_PATH)
+
         # Apply train/val split, store the DataFrame and the
         # corresponding PyTorch Dataset
-        self.update_df(df)
+        self.update_df(train_df)
+        self.update_df(test_df, test=True)
 
-    def update_df(self, df):
+    def update_df(self, df, test=False):
         """
         Update the DataFrame and PyTorch Dataset with the given one
         """
-        # Update the DataFrame
-        self.dataframe = df
+        if test:
+            self.test_df = df
+            self.test_dataset = SquadTorchDataset(self.test_df)
+        else:
+            # Update the DataFrame
+            self.dataframe = df
 
-        # Store a random subset of the entire DataFrame
-        if self.df_percentage < 1.0:
-            self.dataframe = self._get_portion(self.dataframe)
+            # Store a random subset of the entire DataFrame
+            if self.df_percentage < 1.0:
+                self.dataframe = self._get_portion(self.dataframe)
 
-        # Split the DataFrame into train/validation
-        self.train_df, self.val_df = self._train_val_split(self.dataframe)
+            # Split the DataFrame into train/validation
+            self.train_df, self.val_df = self._train_val_split(self.dataframe)
 
-        # Save PyTorch Dataset instances
-        self.train_dataset = SquadTorchDataset(self.train_df)
-        self.val_dataset = SquadTorchDataset(self.val_df)
+            # Save PyTorch Dataset instances
+            self.train_dataset = SquadTorchDataset(self.train_df)
+            self.val_dataset = SquadTorchDataset(self.val_df)
 
     def _add_end_index(self, df):
         """
         Function that takes as input a partially-built SQuAD DataFrame 
-        (with at least ['context', 'text', 'answer_start'] columns)
+        (with at least ['context', 'answer', 'answer_start'] columns)
         and returns the same DataFrame with the new column 'answer_end',
         that consists of last answer character index
         """
@@ -85,33 +94,42 @@ class SquadDataset:
             ans_end.append(s + len(t))
         df["answer_end"] = ans_end
         return df
+    
+    def _remove_duplicated_answers(self, df):
+        """
+        Remove duplicated rows from the given DataFrame
+        """
+        return df.drop_duplicates()
 
-    def _load_dataset(self):
+    def _load_dataset(self, dataset_path, dataframe_path):
         """
         Loads the SQuAD dataset into a Pandas DataFrame, starting from the training set JSON
         """
+        if os.path.exists(dataframe_path):
+            return pd.read_pickle(dataframe_path)
+
         # Read JSON file
-        file = json.loads(open(self.training_set_path).read())
+        file = json.loads(open(dataset_path).read())
 
         # Flatten JSON
-        js = pd.json_normalize(file, self.JSON_RECORD_PATH, meta=[["data", "title"]])
-        m = pd.json_normalize(
+        df = pd.json_normalize(file, self.JSON_RECORD_PATH, meta=[["data", "title"]])
+        df_questions = pd.json_normalize(
             file, self.JSON_RECORD_PATH[:-1], meta=[["data", "title"]]
         )
-        r = pd.json_normalize(
+        df_contexts = pd.json_normalize(
             file, self.JSON_RECORD_PATH[:-2], meta=[["data", "title"]]
         )
 
         # Build the flattened Pandas DataFrame
-        idx = np.repeat(r["context"].values, r.qas.str.len())
-        ndx = np.repeat(m["id"].values, m["answers"].str.len())
-        m["context"] = idx
-        js["q_idx"] = ndx
-        df = pd.concat(
-            [m[["id", "question", "context"]].set_index("id"), js.set_index("q_idx")],
-            axis=1,
-            sort=False,
-        ).reset_index()
+        contexts = np.repeat(df_contexts["context"].values, df_contexts.qas.str.len())
+        contexts = np.repeat(contexts, df_questions["answers"].str.len())
+        df["context"] = contexts
+        df["question_id"] = np.repeat(
+            df_questions["id"].values, df_questions["answers"].str.len()
+        )
+        df["question"] = np.repeat(
+            df_questions["question"].values, df_questions["answers"].str.len()
+        )
         df["context_id"] = df["context"].factorize()[0]
 
         # Rename columns
@@ -122,9 +140,12 @@ class SquadDataset:
 
         # Order columns
         df = df[self.COLUMNS]
-
+        
+        # Remove duplicate answers
+        df = self._remove_duplicated_answers(df)
+        
         # Save the dataframe to a pickle file
-        df.to_pickle(self.dataframe_path)
+        df.to_pickle(dataframe_path)
 
         return df
 
